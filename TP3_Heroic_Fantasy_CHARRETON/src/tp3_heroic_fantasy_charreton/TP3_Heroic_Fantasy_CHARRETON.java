5bfb053b3e2dc745f88f68126dd346efee169317
Tess Charreton <tess.charreton@epfedu.fr>
2024-11-26 17:55:06 +0100

    Création d'un nouveau projet versionné

---
+++ TP3_Heroic_Fantasy_CHARRETON/src/tp3_heroic_fantasy_charreton/TP3_Heroic_Fantasy_CHARRETON.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp3_heroic_fantasy_charreton;
+
+import Armes.Baton;
+import Armes.Epee;
+
+/**
+ *
+ * @author tessc
+ */
+public class TP3_Heroic_Fantasy_CHARRETON {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Epee Excalibur = new Epee(7,5,"Excalibur");
+       Epee Durandal = new Epee(4,7,"Durandal");
+       Baton Charme = new Baton(5,6,"Charme");
+       Baton Chene = new Baton(4,5,"Chêne");
+    }
+    
+}
